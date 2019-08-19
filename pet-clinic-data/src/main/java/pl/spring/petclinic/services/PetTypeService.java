b572de69a67100a22ab18b42708b18691e4d916c
Anna Wojcik <aaswieboda@gmail.com>
2019-08-19 11:50:15 +0200

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/pl/spring/petclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package pl.spring.petclinic.services;
+
+import pl.spring.petclinic.model.PetType;
+
+/**
+ * Created by A.Wójcik on 19.08.2019.
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
