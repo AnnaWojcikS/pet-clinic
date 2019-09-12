2317ad6672628312eb9d50fd8b0b51add111ded0
Anna Wojcik <aaswieboda@gmail.com>
2019-09-12 13:03:01 +0200

    Creating Visit Entity. Closes #38

--- pet-clinic-data/src/main/java/pl/spring/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/pl/spring/petclinic/model/Visit.java
@@ -1,14 +1,21 @@
 package pl.spring.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by A.Wójcik on 19.08.2019.
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
