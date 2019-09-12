680125620f174c12f63b849aaf35c7b563c8e4fb
Anna Wojcik <aaswieboda@gmail.com>
2019-09-12 11:20:28 +0200

    Updating Vet with JPA mapping. Closes #37

--- pet-clinic-data/src/main/java/pl/spring/petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/pl/spring/petclinic/model/Specialty.java
@@ -1,10 +1,17 @@
 package pl.spring.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by A.Wójcik on 19.08.2019.
  */
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
