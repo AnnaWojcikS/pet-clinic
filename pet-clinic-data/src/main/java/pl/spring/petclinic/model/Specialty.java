8697d21065d909d681bef107f852a20b8fdc4a71
Anna Wojcik <aaswieboda@gmail.com>
2019-09-18 10:57:25 +0200

    Adding project Lombok. Closes #51

--- pet-clinic-data/src/main/java/pl/spring/petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/pl/spring/petclinic/model/Specialty.java
@@ -1,5 +1,7 @@
 package pl.spring.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -7,18 +9,15 @@ import javax.persistence.Table;
 /**
  * Created by A.Wójcik on 19.08.2019.
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialties")
 public class Specialty extends BaseEntity {
 
     @Column(name = "description")
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- pet-clinic-data/src/main/java/pl/spring/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/pl/spring/petclinic/model/Visit.java
@@ -1,11 +1,18 @@
 package pl.spring.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by A.Wójcik on 19.08.2019.
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "visits")
 public class Visit extends BaseEntity {
@@ -20,25 +27,5 @@ public class Visit extends BaseEntity {
 
     public LocalDate getDate() {
         return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
     }
 }
